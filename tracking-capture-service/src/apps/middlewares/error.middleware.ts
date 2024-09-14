33e4a1e005464fe7ce8dbfef4f5799302c7e39a7
tuancamtbtx <nguyenvantuan140397@gmail.com>
2024-09-14 23:43:42 +0700

    [add] middleware error

--- tracking-capture-service/src/apps/middlewares/error.middleware.ts
+++ tracking-capture-service/src/apps/middlewares/error.middleware.ts
@@ -17,5 +17,10 @@ export function errorDebugMiddleware(err, req, res, next) {
 
 export function errorReleaseMiddleware(err, req, res, next) {
   console.error(err.stack)
-  res.status(500).json({ message: 'Internal Server Error' })
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+
 }
